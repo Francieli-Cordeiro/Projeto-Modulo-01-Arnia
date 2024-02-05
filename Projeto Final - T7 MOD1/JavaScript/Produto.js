4acafd5b178e178c7220e247a368ae32aeb79a95
Francieli-Cordeiro <152501477+Francieli-Cordeiro@users.noreply.github.com>
2024-02-05 19:07:24 -0300

    05-02

---
+++ Projeto Final - T7 MOD1/JavaScript/Produto.js
@@ -0,0 +1,14 @@
+const meusDados = () => {
+    window.location = '../Html/meuPerfil.html'
+}
+
+const meusResgates = () => {
+    window.location = '../Html/MeusResgates.html'
+}
+
+const minhasJoias = () => {
+    window.location = '../Html/MinhasJoias.html'
+}
+
+const sair = () => {
+    window.location = '../Html/Index.html'
